961d30e44191a2c7292dced70bf6bfe049bdbddc
Mark Lavrentyev <lavrema@outlook.com>
2017-04-04 15:02:12 -0400

    Add scaling methods and general method

--- datagenerator.py
+++ datagenerator.py
@@ -4,11 +4,29 @@ import numpy as np
 class _DataGenerator(object):
     def genTrainData(self):
         data = []
-        with open('train-data.csv', 'b') as f:
+        with open('../train-data.csv', 'r') as f:
             data = [list(map(int,rec)) for rec in csv.reader(f, delimiter=',')]
 
         data = np.array(data)
         labels = data[:,0]
-        np.delete(data, 0, 1)
+        data = np.delete(data, 0, 1)
 
         return labels, data
+    
+    def scaleData(self, data):
+        return data/255
+
+    def labelsToOutputs(self, labels):
+        outputs = np.zeros((labels.size, 10))
+
+        for i in range(labels.size):
+            outputs[i,labels[i]] = 1
+
+        return outputs
+        
+    def getInputsOutputs(self):
+        labels, data = self.genTrainData()
+        inputs = self.scaleData(data)
+        outputs = self.labelsToOutputs(labels)
+
+        return inputs, outputs
