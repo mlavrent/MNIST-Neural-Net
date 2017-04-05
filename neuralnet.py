87a0c5fafee93793754849b4e8f3c4a175f8aaa0
Mark Lavrentyev <lavrema@outlook.com>
2017-04-05 13:22:32 -0400

    Add back propogation method

--- neuralnet.py
+++ neuralnet.py
@@ -31,9 +31,46 @@ class NeuralNet(object):
     def sigmoid(self, x):
         return 1/(1+np.exp(-x))
 
+    def costFunction(self, aOutValues, rOutValues):
+        # aOutValues - [. . .] (outputLayerSize)
+        # rOutValues - [. . .] (outputLayerSize)
+        total = 0
+        for i in range(self.outputLayerSize):
+            total += 0.5*(rOutValues[i] - aOutValues[i])**2
+
+        return total
+
+    def backProp(self, inValues, rOutValues):
+        aOutValues = self.forwardProp(inValues)
+
+        #Iterate over h2_o_weights
+        dC_dh2o_w = np.zeros((self.h2LayerSize, self.outputLayerSize))
+        for o in range(self.outputLayerSize):
+            for h2 in range(self.h2LayerSize):
+                dC_dh2o_w[h2][o] = -(rOutValues[o] - aOutValues[o]) * aOutValues[o]*(1-aOutValues[o]) * self.h2Val[h2]
+
+        #Iterate over h1_h2_weights
+        dC_dh1h2_w = np.zeros((self.h1LayerSize, self.h2LayerSize))
+        for h2 in range(self.h2LayerSize):
+            for h1 in range(self.h1LayerSize):
+                dC_dh1h2_w[h1][h2] = dC_dh2 * self.h2Val[h2]*(1-self.h2Val[h2]) * self.h1Val[h1]
+                #TODO: calculate dC_dh2
+
+        #Iterate over i_h1_weights
+        dC_dih1_w = np.zeros((self.inputLayerSize, self.h1LayerSize))
+        for h1 in range(self.h1LayerSize):
+            for i in range(self.inputLayerSize):
+                dC_dih1_w[i][h1] = dC_dh1 * self.h1Val[h1]*(1-self.h1Val[h1]) * self.inputVal[i]
+                #TODO: calculate dC_dh1
+
+        return {"i_to_h1": dC_dih1_w,
+                "h1_to_h2": dC_dh1h2_w,
+                "h2_to_o": dC_dh2o_w}
+
 
 dg = _DataGenerator()
 inputs, outputs = dg.getInputsOutputs()
 
 nn = NeuralNet(784, 15, 15, 10)
-print(nn.forwardProp(inputs))
+
+nn.backProp(inputs[0], outputs[0])
