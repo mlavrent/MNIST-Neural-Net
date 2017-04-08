de672ae59fde36324341fbf08fb5e311c5a458a8
Mark Lavrentyev <lavrema@outlook.com>
2017-04-08 15:46:07 -0400

    Write class to save and load weights to not retrain nn every time

---
+++ weightsaver.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+class WeightSaverLoader(object):
+
+    def saveWeights(self, pathToFolder, *args):
+        # *args are the np array weights in order
+        fileName = ""
+
+        #Generate file name for npz archive
+        for array in args:
+            if fileName != "":
+                fileName += "_"
+                
+            fileName += str(array.shape[0])
+        fileName += "_" + str(args[len(args) - 1].shape[1])
+        fileName = pathToFolder + fileName + ".npz"
+ 
+        np.savez(fileName, *args)
+
+    def loadWeights(self, file, nnArray):
+        #weightArray is an array of the number of neurons in each layer
+        loadedFile = np.load(file)
+
+        #Error checking for equal number of layers
+        if len(nnArray)-1 != len(loadedFile.items()):
+            return -1
+
+        allWeights = [loadedFile["arr_" + str(num)] for num in range(len(loadedFile.items()))]
+
+        #Error checking for non-matching imported values and needed sizes
+        for i in range(len(allWeights)):
+            if allWeights[i].shape[0] != nnArray[i]:
+                return -1
+        if allWeights[len(allWeights) - 1].shape[1] != nnArray[len(nnArray) - 1]:
+            return -1
+            
+        loadedFile.close()
+        
+        return allWeights
+          
+wsl = WeightSaverLoader()
+
+wsl.saveWeights("weightData/", np.array([[1,2], [2,3], [4,5]]), np.array([[5], [4]]))
+
+a = wsl.loadWeights("weightData/3_2_1.npz", [3, 2, 1])
